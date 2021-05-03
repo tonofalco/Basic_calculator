bf25399b84da998253c4c2a752f3ba842da7a357
Antonio <tonofalcon22@gmail.com>
2021-05-02 20:22:52 -0500

    primer commit xd

---
+++ js/darkmode.js
@@ -0,0 +1,26 @@
+
+const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click', () => {
+	document.body.classList.toggle('dark');
+	btnSwitch.classList.toggle('active');
+
+	// Guardamos el modo en localstorage.
+	if(document.body.classList.contains('dark')){
+		localStorage.setItem('dark-mode', 'true');
+	} else {
+		localStorage.setItem('dark-mode', 'false');
+	}
+});
+
+// Obtenemos el modo actual.
+if(localStorage.getItem('dark-mode') === 'true'){
+	document.body.classList.add('dark');
+	btnSwitch.classList.add('active');
+    // document.body.style.transition= "0s ease all"	
+} else {
+	document.body.classList.remove('dark');
+	btnSwitch.classList.remove('active');
+    document.body.style.transition= ".8s ease all"
+}
+
